8f10b4c92718b48a811e0769fdb0d9497f17cd52
denisdavydov <denises.krotic@ya.ru>
2026-09-14 21:40:32 +0500

    Revert "test(update): MUTANT M4, retire never removes the bundle swapped out but says it did"
    This reverts commit d09c1f9c5eacf14b5cf429cfa6c78a62a816d4e0.
    
    Signed-off-by: denisdavydov <denises.krotic@ya.ru>

--- internal/supervisor/update.go
+++ internal/supervisor/update.go
@@ -415,6 +415,9 @@ func (t *Takeover) retireOnce() (removed, again bool, why error) {
 	if err := t.retirable(); err != nil {
 		return false, false, err
 	}
+	if err := os.RemoveAll(t.Staged); err != nil {
+		return false, true, fmt.Errorf("removing it failed: %w", err)
+	}
 	return true, false, nil
 }
 
